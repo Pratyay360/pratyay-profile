4dc92fcaa48a6da881fc6038cb27185f206b58c2
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-10 01:43:48 +0530

    Final breathing final form

--- src/app/Backgroundcircles.tsx
+++ src/app/Backgroundcircles.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 export default function Backgroundcircles() {
   return (
-    <div className="relative flex justify-center items-center">
+    <div className="relative flex flex-wrap justify-center items-center">
         <div className="absolute border border-gray-500 rounded-full h-[200px] w-[200px] mt-52 animate-bounce"/>
         <div className="absolute border border-red-500 rounded-full h-[300px] w-[300px] mt-52 animate-pulse"/>
         <div className="absolute border border-orange-500 rounded-full h-[500px] w-[500px] mt-52 animate-bounce" />
--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div>
  <h1 className="text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl">CONTACT ME</h1>
-    <div className="flex high flex-row items-center justify-center overflow-hidden tracking-[10px] snap-mandatory text-center mb-5">
+    <div className="flex high flex-row items-center justify-center overflow-hidden tracking-[10px] snap-mandatory text-center mb-5 mr-5">
       {social.map((icon, index) => (
         <div key={index} className="cont transform transition-all hover:scale-110" >
           <a href={icon.link}>
