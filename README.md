7c28c838484bbbe446e76d618d6ef5387872fb22
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-10 00:59:13 +0530

    Final breathing final form

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # This is the personal social media link sharing website of mine.
 ## I have created this website with next js
 
+# [link](https://pratyay.vercel.app/)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Pratyay',
+  title: 'Pratyay Mitra Mustafi',
   description: 'This is portfollio website of Pratyay Mustafi',
 }
 
