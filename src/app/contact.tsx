e2a0e607fd6b014fbc6f5d9fb4589ee459509842
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-10 08:42:05 +0530

    Final breathing final form

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 export default function Contact() {
   return (
     <div>
- <h1 className="flex text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl">CONTACT ME</h1>
+ <h1 className="flex shrink text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl mr-2 ml-2">CONTACT ME</h1>
     <div className="flex high flex-row items-center justify-center overflow-hidden tracking-[10px] snap-mandatory text-center mb-5 mr-5">
       {social.map((icon, index) => (
         <div key={index} className="cont transform transition-all hover:scale-110" >
