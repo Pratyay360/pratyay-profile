2269e5db15c37b2336ca133991b571f38d135200
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-10 14:59:16 +0530

    add cert

--- src/app/certificates.tsx
+++ src/app/certificates.tsx
@@ -7,13 +7,13 @@ export default function Certificates() {
         <div className="flex flex-column text-center items-center justify-center lg:text-3xl">
           <h1 className="top-36 tracking-[20px] text-gray-500 ml-2">CERTIFICATES</h1>
         </div>
-            <section className="text-gray-300 body-font">
+        <section className="text-gray-300 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap -m-4">
                         {cards.map((card, index) => (
                             <div className="p-4 md:w-1/3" key={index}>
                                 <a href={card.link} className="block" target="_blank" rel="noopener noreferrer">
-                                    <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110">
+                                    <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
                                         <img
                                             className="lg:h-48 md:h-36 w-full object-cover object-center"
                                             src={card.imageSrc}
