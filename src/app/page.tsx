65e28a59d104e7f2b15c7f170939d933b7b4883d
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-17 18:29:09 +0530

    Sco1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
   return (
         <>
         <header>
-        <meta name="google-site-verification" content="4Cj3JxMGPpb2BMCwVMthhNKLyH9CyKeJxR0jdChlx5E" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <meta name="description" content="Pratyay Mitra Mustafi's Portfolio" />
