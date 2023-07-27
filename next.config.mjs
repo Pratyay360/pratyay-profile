3c21eec58303348737231f13d681bf1484cb0d93
Pratyay360 <pratyaymustafi@gmail.com>
2023-07-28 00:52:55 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,13 @@ import million from "million/compiler";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   images: {
 
     domains: ['raw.githubusercontent.com','github.com', 'wekwttnnowtwqzntesch.supabase.co'],
   },
 };
-export default million.next(nextConfig);
+const supabase = {
+  persistSession: false,
+}
+export default million.next(nextConfig, supabase);
