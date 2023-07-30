fd31cce46b40eca2b8f1f4be1ca472fae410aa73
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-30 12:48:39 +0530

    added analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from "react"
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './navbar/navbar'
 import Projects from './normal_pages/projects'
 import Contact from './normal_pages/contact'
@@ -11,6 +12,7 @@ import Certificates from './normal_pages/certificates'
 export default function Home() {
   return (
         <>
+        <Analytics/>
         <header>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
