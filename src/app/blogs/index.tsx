9c54f5a18b3e78464bee924410c4e2c07fb101bd
Pratyay360 <pratyaymustafi@gmail.com>
2023-08-17 19:12:42 +0000

    solved typo errors

--- src/app/blogs/index.tsx
+++ src/app/blogs/index.tsx
@@ -49,7 +49,7 @@ const blo = block(function Blog() {
                         <div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
                             {posts.map((c, index) => (
                                 <div className="p-4 md:w-1/3" key={index}>
-                                    <a href={`https://pratyaywrites.hashnode.dev//${c.slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
+                                    <a href={`https://pratyaywrites.hashnode.dev/${c.slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
                                         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
                                             <Image
                                                 className="lg:h-48 md:h-36 w-full object-cover object-center"
