7597667ea9e270b6f3fb0024a5776baf06752c99
Pratyay360 <pratyaymustafi@gmail.com>
2023-09-26 18:10:23 +0530

    resized image

--- src/app/animation/index.tsx
+++ src/app/animation/index.tsx
@@ -4,10 +4,12 @@ import { motion } from 'framer-motion'
 const backgr =  block(function Backgroundcircles() {
   return (
     <div className="relative flex flex-wrap justify-center items-center">
-        <div className="absolute border border-gray-500 rounded-full h-[200px] w-[200px] mt-52 animate-bounce"/>
-        <div className="absolute border border-red-500 rounded-full h-[300px] w-[300px] mt-52 animate-pulse"/>
+        <div className="absolute border border-gray-500 rounded-full h-[330px] w-[330px] mt-52 animate-bounce"/>
+        <div className="absolute border border-red-500 rounded-full h-[400px] w-[400px] mt-52 animate-pulse"/>
         <div className="absolute border border-orange-500 rounded-full h-[500px] w-[500px] mt-52 animate-bounce" />
-        <div className="absolute rounded-full border border-yellow-500 opacity-20 h-[650px] w-[650px] animate-pulse"/>
+        <div className="absolute rounded-full border border-yellow-500 opacity-50 h-[650px] w-[650px] animate-pulse"/>
+        <div className="absolute rounded-full border border-green-700 opacity-30 h-[700px] w-[700px] animate-bounce"/>
+        <div className="absolute rounded-full border border-white-500 opacity-20 h-[850px] w-[850px] animate-pulse"/>
         <div   />
     </div>
   )
