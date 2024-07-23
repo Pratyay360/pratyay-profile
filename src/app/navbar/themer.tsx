7a7a0726ea0cb578b3e116de089513ed9bcb1464
Pratyay Mustafi <pratyaymustafi@gmail.com>
2024-07-24 01:31:33 +0530

    simple changes

--- src/app/navbar/themer.tsx
+++ src/app/navbar/themer.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import * as React from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
@@ -6,20 +7,16 @@ import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
-  
+
   const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
-    <Button onClick={toggleTheme} variant="outline" size="icon" className="border-gray-200">
-      {theme === "dark" ? (
-        <SunIcon className="h-[1.2rem] w-[1.2rem] " />
-      ) : (
-        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
-      )}
-        <span className="sr-only">Toggle theme</span>
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
     </Button>
-
   );
 }
