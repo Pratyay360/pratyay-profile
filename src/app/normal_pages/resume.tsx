9e3a992b88658f84640e133e9e60477f7e87a8a6
Pratyay360 <pratyaymustafi@gmail.com>
2024-12-31 13:31:06 +0530

    package updates

--- src/app/normal_pages/resume.tsx
+++ src/app/normal_pages/resume.tsx
@@ -1,16 +1,28 @@
-import Link from 'next/link';
+import Link from "next/link";
 export default function Resume() {
-return (
-        <>
-            <h1 className="text-center items-center justify-center top-36 tracking-[20px] dark:text-gray-500 text-3xl lg:text-4xl font-bold">RESUME</h1>
-            <div className="text-center items-center justify-center top-36 backdrop-blur-30 py-20 ">
-                <Link href={'https://drive.google.com/file/d/1Kb_cOhevNgiif-lV3LPJFPjStCKEd0dt/view?usp=drive_link' || ''}>
-                <button role="button_open_resume" className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-large text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500
+  return (
+    <>
+      <h1 className="text-center items-center justify-center top-36 tracking-[20px] dark:text-gray-500 text-3xl lg:text-4xl font-bold">
+        RESUME
+      </h1>
+      <div className="text-center items-center justify-center top-36 backdrop-blur-30 py-20 ">
+        <Link
+          href={
+            "https://drive.google.com/file/d/1Kb_cOhevNgiif-lV3LPJFPjStCKEd0dt/view?usp=drive_link"
+          }
+        >
+          <button
+            role="button_open_resume"
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-large text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500
                  group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white/30 dark:text-white focus:ring-4 focus:outline-none
-                 focus:ring-purple-200 dark:focus:ring-purple-800 transform-gpu transition-all hover:scale-110">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 transform-gpu lg:text-3xl hover:scale-110">Show Resume</span>
-                </button></Link>
-            </div>
-        </>
-    );
-};
+                 focus:ring-purple-200 dark:focus:ring-purple-800 transform-gpu transition-all hover:scale-110"
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 transform-gpu lg:text-3xl hover:scale-110">
+              Show Resume
+            </span>
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+}
