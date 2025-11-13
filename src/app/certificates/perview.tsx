857d4c8f2ab70c1ae3ffa88a2d0be480ae7dc4ab
PRATYAY MUSTAFI <pratyaymustafi@outlook.com>
2025-11-14 01:21:15 +0530

    Refactor certificate rendering logic in perview.tsx

--- src/app/certificates/perview.tsx
+++ src/app/certificates/perview.tsx
@@ -14,18 +14,20 @@ export default async function Certificates() {
     loading = true;
   }
   return (
-    <>
+   <>
       <SkeletonTheme baseColor="#e0e0e0" highlightColor="#f5f5f5">
         <div className="flex flex-column text-center items-center justify-center">
           <h1 className="text-center items-center justify-center top-36 tracking-[20px] text-2xl/3 lg:text-4xl font-bold p-3 m-2 overflow-auto">
             CERTIFICATES
           </h1>
         </div>
+
         {loading && (
           <div className="p-10 mt-10">
             <Skeleton height={500} count={1} />
           </div>
         )}
+
         <section className="body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
@@ -40,25 +42,29 @@ export default async function Certificates() {
                       />
                     </div>
                   ))
-                : certificate?.slice(0, 3).map((cer, index) => (
-                    <div className="p-4 md:w-1/3" key={index}>
-                      <CertCard
-                        link={cer.link}
-                        imageSrc={cer.imageSrc}
-                        description={cer.description}
-                        title={cer.title}
-                      />
+                : (
+                    <>
+                      {certificate?.slice(0, 3).map((cer, index) => (
+                        <div className="p-4 md:w-1/3" key={index}>
+                          <CertCard
+                            link={cer.link}
+                            imageSrc={cer.imageSrc}
+                            description={cer.description}
+                            title={cer.title}
+                          />
+                        </div>
+                      ))}
                       {!loading && (
-                        <center>
+                        <div className="w-full mt-6 text-center">
                           <Link href="/certificates">
                             <button className="button-30" role="button_more_certificate">
                               See More
                             </button>
                           </Link>
-                        </center>
+                        </div>
                       )}
-                    </div>
-                  ))}
+                    </>
+                  )}
             </div>
           </div>
         </section>
